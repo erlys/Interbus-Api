e39a2486c960257f43e7bbedd5bdf69e4a296456
Erlys Oliveros <oliveros.vanessa.17@gmail.com>
2022-05-15 09:53:11 -0400

    Implementado autenticacion

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function doLogin(Request $request)
+    {    
+        $credentials=$request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {            
+            return response()->json([
+                "status" => true,
+                "message" => "Bienvenido al sistema"
+            ]);
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "Usuario y/o contrasena incorrecta"
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BusController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +14,5 @@ Route::prefix('v1/')->group(function () {
     Route::get('buses',  [BusController::class,    'index']);
     Route::post('ticket/validate', [TicketController::class, 'validateCode']);
     Route::post('ticket/mark', [TicketController::class, 'markTicket']);
+    Route::post('login', [UserController::class, 'markTicket']);
 });
